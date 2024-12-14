59334ea5541b58820d62062d2e597e22a2467630
stephen <chung25stephen@gmail.com>
2024-12-14 00:25:34 +0000

    add explicit animation pokeball

--- lib/widgets/home/home.dart
+++ lib/widgets/home/home.dart
@@ -3,9 +3,31 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_template/providers/selected_pokemon.dart';
 import 'package:flutter_template/widgets/pokemons/pokemon_home_card.dart';
+import 'dart:math' as math;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+
+  late final AnimationController _controller;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+  }
+
   @override
   Widget build(BuildContext context) {
     final selectedPokemon = context.watch<SelectedPokemonProvider>().selectedPokemon;
@@ -22,10 +44,30 @@ class HomePage extends StatelessWidget {
                       PokemonHomeCard(pokemon: selectedPokemon),
                       Positioned(
                         right: 0,
-                        child: IconButton(
-                          iconSize: 35,
-                          onPressed: () => context.push('/pokemons'),
-                          icon: const Icon(Icons.change_circle_outlined),
+                        child: AnimatedBuilder(
+                          animation: _controller,
+                          builder: (_, child) {
+                            if (_controller.isAnimating) {
+                              return Transform.rotate(
+                                angle: -_controller.value * 2 * math.pi,
+                                child: child,
+                              );
+                            } 
+                            return child!;
+                          },
+                          child: IconButton(
+                            iconSize: 35,
+                            onPressed: () {
+                              _controller.repeat();
+                              Future.delayed(const Duration(seconds: 1), () {
+                                _controller.stop();
+                                if (context.mounted){
+                                  context.push('/pokemons');
+                                }
+                              });
+                            },
+                            icon: const Icon(Icons.catching_pokemon_outlined),
+                          ),
                         ),
                       ),
                     ],
